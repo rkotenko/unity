0a743760bd9aaa26b86dd26a77544d555223995f
rkotenko <robkotenko@gmail.com>
2016-02-07 23:49:22 -0500

    Number Wizard Complete
     First “game” made for unity.  Really, it was just for everyone to
    learn a few programming basics.  Of benefit to me was learning about
    input in Unity.

--- Number Wizard/Assets/scripts/NumberWizard.cs
+++ Number Wizard/Assets/scripts/NumberWizard.cs
@@ -2,14 +2,44 @@
 using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
-
+  int min;
+  int max;
+  int guess = 500;
+  
 	// Use this for initialization
-	void Start () {
-		print ("Welcome to Number Wizard");
+	void Start() {
+		min = 1;
+		max = 1000;
+    print ("Welcome to Number Wizard");
+		print ("Pick a number in your head between " + min + " and " + max);
+		
+		max += 1;
+		askQuestion();
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update() {
+		
+		if(Input.GetKeyDown(KeyCode.UpArrow)) {
+			//print ("Up was pressed");	
+      min = guess;	
+      askQuestion();
+		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			//print ("Down was pressed");
+      max = guess;
+      askQuestion();
+		} else if (Input.GetKeyDown(KeyCode.Return)) {
+			print ("I won!");
+      print("========================================");
+      Start();
+		}
+    
 	}
+  
+  void askQuestion() {
+    guess = (min + max) / 2;
+    print ("Is the number higher or lower than " + guess + "?");
+    print ("Up for higher, down for lower, and return for equals");  
+  }
 }
